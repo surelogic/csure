8ccd5e60a97b4b8bb6b301c4c357cea149309428
edwin <edwin.chan@surelogic.com>
2015-06-01 10:45:48 -0400

    Restored example files

---
+++ stuff/csure-example/test.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+class [[valueObject]] Point {
+	const int x, y;
+
+public:
+	Point(int i, int j) : x(i), y(j) {
+		// Nothing else to do
+	}
+
+	[[starts("nothing")]] int getX() {
+		return x;
+	}
+
+	[[starts("nothing")]] int getY() {
+		return y;
+	}
+};
+
+class [[referenceObject]] UniqueID {
+
+};
+
+int main(void) {
+  Point p(1, 2);
+  return p.getX();
+}
+
