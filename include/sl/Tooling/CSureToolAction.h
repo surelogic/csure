867a6e93d128dcfe1f176c7a121760ceff0c9e73
Tim Halloran <tim.halloran@surelogic.com>
2015-06-12 13:58:20 -0400

    Working

---
+++ include/sl/Tooling/CSureToolAction.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "clang/AST/ASTConsumer.h"
+#include "clang/Frontend/FrontendActions.h"
+
+class CSureToolAction : public clang::PluginASTAction {
+protected:
+	// this gets called by Clang when it invokes CSure
+	std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &CI, llvm::StringRef file);
+
+	// implement this function if you want to parse custom cmd-line args
+	bool ParseArgs(const clang::CompilerInstance &CI, const std::vector<std::string> &args);
+
+	virtual  bool BeginSourceFileAction(clang::CompilerInstance &CI, llvm::StringRef Filename) override;
+};
+
---
+++ lib/Tooling/CSureToolAction.cc
@@ -0,0 +1,20 @@
+#include "sl/Tooling/CSureToolAction.h"
+
+std::unique_ptr<clang::ASTConsumer> CSureToolAction::CreateASTConsumer(clang::CompilerInstance &CI, llvm::StringRef file) {
+	//TODO CSurePreprocessorInfo* preprocessor_consumer = new CSurePreprocessorInfo();
+
+	CI.getPreprocessor().addPPCallbacks(std::unique_ptr<clang::PPCallbacks>(preprocessor_consumer));
+	return std::unique_ptr<clang::ASTConsumer>(new ExampleASTConsumer(&CI));
+}
+
+bool CSureToolAction::ParseArgs(const clang::CompilerInstance &CI, const std::vector<std::string> &args) {
+	llvm::errs() << "** Working dir? " << CI.getFileSystemOpts().WorkingDir << "\n";
+	llvm::errs() << "** Output going to " << CI.getFrontendOpts().OutputFile << "\n";
+	return true;
+}
+
+bool CSureToolAction::BeginSourceFileAction(clang::CompilerInstance &CI,
+	llvm::StringRef Filename) {
+	llvm::errs() << "** Looking at " << Filename << "\n";
+	return PluginASTAction::BeginSourceFileAction(CI, Filename);
+}
