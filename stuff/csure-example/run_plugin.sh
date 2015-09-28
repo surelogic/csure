fc8da2978879e9b65dd5884c37a18354669bde81
Tim Halloran <hallorant@gmail.com>
2015-09-28 16:56:41 -0400

    Deleted old file

--- stuff/csure-example/run_plugin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# old setup
-../../../../../llvm-360-build/bin/clang++ -std=c++11 -Xclang -load \
-      -Xclang ../../../../../llvm-360-build/lib/CSure.so \
-      -Xclang -plugin -Xclang -example-plugin \
-      -Wall -c test.cc
