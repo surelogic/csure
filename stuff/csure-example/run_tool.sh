bb76f9f9ec5952215d33d182a4bf3a70999e5cc2
Tim Halloran <tim.halloran@surelogic.com>
2015-09-28 17:16:58 -0400

    Working on upgrade to clang 3.7.0

--- stuff/csure-example/run_tool.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-${LLVMBUILD}/bin/surelogic-csure test.cc -- ${LLVMBUILD}/bin/clang++ \
- -I /opt/local/include/gcc49/c++ \
- -I /opt/local/include/gcc49/c++/x86_64-apple-darwin12 \
- -I /opt/local/include/gcc49/c++//backward \
- -std=c++14 -c
