7b8ba64127b860dc79e3b32c8f008fcebe2d7791
Tim Halloran <hallorant@gmail.com>
2015-10-06 14:08:11 -0400

    Deleted files

--- stuff/csure-example/run_tool.mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Make sure build/bin is in your path (like for clang-format)
-surelogic-csure \
- test.cc -- clang++ -std=c++14 -c \
- -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1 \
- -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/6.1.0/include \
- -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include \
- -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include \
- -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks
